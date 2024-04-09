5c79228eccfafde907297100834287ee14d34f4e
AgustinGallardo <agustingallardo97@outlook.com.ar>
2024-04-09 01:46:29 -0300

    Primer Programa en Java

---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Excelentes noticias el codigo funciona \n No puedo decir lo mismo de mis riñones");
+    }
+}
